c930f894845f115f9b2488fe6e2400d8a37fa9d0
smarrerof <smarrerof@gmail.com>
2017-04-03 14:55:07 +0100

    Updated Dtos

--- Data/Dtos/FeatureDto.cs
+++ Data/Dtos/FeatureDto.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace WebApplicationBasic.Data.Dtos
+﻿namespace WebApplicationBasic.Data.Dtos
 {
     public class FeatureDto
     {
-        public int IdFeature { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
     }
 }
--- Data/Dtos/MakeDto.cs
+++ Data/Dtos/MakeDto.cs
@@ -1,15 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace WebApplicationBasic.Data.Dtos
 {
     public class MakeDto
     {
-        public int IdMake { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
-        public List<ModelDto> Models { get; set; }
+        public ICollection<ModelDto> Models { get; set; }
+
+        public MakeDto()
+        {
+            Models = new Collection<ModelDto>();
+        }
     }
 }
